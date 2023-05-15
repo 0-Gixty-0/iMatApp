98b8e0c4e48a89c0e3ac5c1ed395610e7c07ae66
0-gixty-0 <erik.andreasson73@gmail.com>
2023-05-15 11:11:14 +0200

    Added Functionality To Category Buttons

--- src/imat/MainViewController.java
+++ src/imat/MainViewController.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
@@ -24,19 +25,14 @@ public class MainViewController implements Initializable {
 
     @FXML
     Label pathLabel;
-
     @FXML
     FlowPane generalItemsFlowPane;
-
     @FXML
     AnchorPane previousPurchasesOverlay;
-
     @FXML
     AnchorPane profileOverlay;
-
     @FXML
     AnchorPane shoppingCartOverlay;
-
     @FXML
     AnchorPane detailViewAnchorPane;
 
@@ -63,7 +59,57 @@ public class MainViewController implements Initializable {
         for (Product item : dataHandler.getProducts())
             generalItemsFlowPane.getChildren().add(productListItemMap.get(item.getName()));
     }
+    // Update productlist with category
+    private void updateProductListCategory(ProductCategory category){
+        generalItemsFlowPane.getChildren().clear();
+        for (Product item : dataHandler.getProducts(category))
+            generalItemsFlowPane.getChildren().add(productListItemMap.get(item.getName()));
+    }
 
+    // Category Button Methods
+    public void showDairyItems(){
+        updateProductListCategory(ProductCategory.DAIRIES);
+    }
+
+    public void showMeatItems(){
+        updateProductListCategory(ProductCategory.MEAT);
+    }
+
+    public void showFishItems(){
+        updateProductListCategory(ProductCategory.FISH);
+    }
+
+    public void showVegetableItems(){
+        updateProductListCategory(ProductCategory.VEGETABLE_FRUIT);
+    }
+
+    public void showFruitItems(){
+        updateProductListCategory(ProductCategory.FRUIT);
+    }
+
+    public void showHerbItems(){
+        updateProductListCategory(ProductCategory.HERB);
+    }
+
+    public void showPastaItems(){
+        updateProductListCategory(ProductCategory.PASTA);
+    }
+
+    public void showNutsItems(){
+        updateProductListCategory(ProductCategory.NUTS_AND_SEEDS);
+    }
+
+    public void showColdDrinksItems(){
+        updateProductListCategory(ProductCategory.COLD_DRINKS);
+    }
+
+    public void showHotDrinksItems(){
+        updateProductListCategory(ProductCategory.HOT_DRINKS);
+    }
+
+    public void showBakingItems(){
+        updateProductListCategory(ProductCategory.FLOUR_SUGAR_SALT);
+    }
 
     // Open / Close Overlays
     public void openPreviousPurchasesOverlay(){
